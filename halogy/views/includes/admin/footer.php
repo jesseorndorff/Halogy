7001825ab84230e27fa9324c8e9ae5ee1d381b1f
Stu Green <stu.green@haloweb.co.uk>
2012-04-23 15:25:21 +0100

    Initial Commit 1.2

---
+++ halogy/views/includes/admin/footer.php
@@ -0,0 +1,28 @@
+
+			<div id="halogycms_browser" class="loading"></div>
+
+		</div>
+
+	</div>
+
+
+	<div id="footer" class="content">
+
+		<div class="container">
+
+		
+			<p class="copyright">Powered by <a href="http://www.halogy.com" title="Halogy">Halogy</a></p>
+			
+			<br />
+			
+			<a href="http://www.halogy.com"><img src="<?php echo $this->config->item('staticPath'); ?>/images/halogy_marque.gif" alt="Powered by Halogy" /></a>
+
+
+		</div>
+
+	</div>
+
+</div>
+	
+</body>
+</html>
