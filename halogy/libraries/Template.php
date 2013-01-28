35c4050ad61abef6fd54d40840f9ab1dcb5ae2b7
jesseorndorff <jesse@jesseorndorff.com>
2013-01-28 13:42:21 +0700

    Added corrected hooks for wiki pages.

--- halogy/libraries/Template.php
+++ halogy/libraries/Template.php
@@ -704,6 +704,38 @@ class Template {
 			}
 		}
 		
+		// get wiki headlines
+		if (preg_match_all('/{headlines:wiki(:category(\(([A-Za-z0-9_-]+)\))?)?(:limit(\(([0-9]+)\))?)?}/i', $body, $matches))
+		{
+			// load wiki model
+			$this->CI->load->model('wiki/wiki_model', 'wiki');
+
+			// filter matches
+			$headlineID = preg_replace('/{|}/', '', $matches[0][0]);
+			$limit = ($matches[3][0]) ? $matches[3][0] : $this->CI->site->config['headlines'];			
+
+			// get latest posts
+			if ($headlines = $this->CI->wiki->get_pages($limit))
+			{	
+				// fill up template array
+				$i = 0;
+				foreach ($headlines as $headline)
+				{
+					
+					$template[$headlineID][$i] = array(
+						'headline:link' => site_url('wiki/' .$headline['uri']),
+						'headline:title' => $headline['pageName'],
+					);
+
+					$i++;
+				}
+			}
+			else
+			{
+				$template[$headlineID] = array();
+			}
+		}
+		
 		// get gallery
 		if (preg_match_all('/{gallery:([A-Za-z0-9_-]+)(:limit\(([0-9]+)\))?}/i', $body, $matches))
 		{
@@ -913,39 +945,6 @@ class Template {
 					$template['headlines:shop:cartitems'] = array();
 				}
 			}
-			
-		// get wiki headlines
-		if (preg_match_all('/{headlines:wiki(:category(\(([A-Za-z0-9_-]+)\))?)?(:limit(\(([0-9]+)\))?)?}/i', $body, $matches))
-		{
-			// load wiki model
-			$this->CI->load->model('wiki/wiki_model', 'wiki');
-
-			// filter matches
-			$headlineID = preg_replace('/{|}/', '', $matches[0][0]);
-			$limit = ($matches[3][0]) ? $matches[3][0] : $this->CI->site->config['headlines'];			
-
-			// get latest posts
-			if ($headlines = $this->CI->wiki->get_pages($limit))
-			{	
-				// fill up template array
-				$i = 0;
-				foreach ($headlines as $headline)
-				{
-					
-					$template[$headlineID][$i] = array(
-						'headline:link' => site_url('wiki/' .$headline['uri']),
-						'headline:title' => $headline['pageName'],
-					);
-
-					$i++;
-				}
-			}
-			else
-			{
-				$template[$headlineID] = array();
-			}
-		}
-
 			
 			// get shop navigation
 			if (preg_match('/({shop:categories((.+)?)})+/i', $body))
