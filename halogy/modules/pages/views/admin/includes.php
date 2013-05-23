e97aa0f54e0ca73460fdbf27994750783452d61d
Jesse Orndorff <jesse@jesseorndorff.com>
2013-05-23 12:31:15 +0700

    update to include page

--- halogy/modules/pages/views/admin/includes.php
+++ halogy/modules/pages/views/admin/includes.php
@@ -1,55 +1,62 @@
-<h1 class="headingleft">Includes</h1>
+<div class="row">
+	<div class="large-12 columns header">
 
-<div class="headingright">
-	<a href="<?php echo site_url('/admin/pages/templates'); ?>" class="button blue">Templates</a>
-	<a href="<?php echo site_url('/admin/pages/includes/css'); ?>" class="button blue">CSS</a>
-	<a href="<?php echo site_url('/admin/pages/includes/js'); ?>" class="button blue">Javascript</a>	
-	<a href="<?php echo site_url('/admin/pages/add_include'); ?>" class="button">Add Include</a>
+		<h1 class="headingleft">Includes</h1>
+
+		<ul class="group-button">
+			<li><a href="<?php echo site_url('/admin/pages/templates'); ?>" class="bluebutton">Templates</a></li>
+			<li><a href="<?php echo site_url('/admin/pages/includes/css'); ?>" class="bluebutton">CSS</a></li>
+			<li><a href="<?php echo site_url('/admin/pages/includes/js'); ?>" class="bluebutton">Javascript</a></li>	
+			<li><a href="<?php echo site_url('/admin/pages/add_include'); ?>" class="green">Add Include</a></li>
+		</ul>
+	</div>
 </div>
 
-<div class="hidden">
-	<p class="hide"><a href="#">x</a></p>
-	<div class="inner"></div>
+<div class="row">
+	<div class="large-12 columns body">
+		<?php if ($includes): ?>
+
+		<?php echo $this->pagination->create_links(); ?>
+
+		<table class="default">
+			<thead>
+				<tr>
+					<th>Reference</th>
+					<th>Date Modified</th>
+					<th class="tiny">&nbsp;</th>
+					<th class="tiny">&nbsp;</th>
+				</tr>
+			</thead>
+		<?php
+			$i = 0;
+			foreach ($includes as $include):
+			$class = ($i % 2) ? ' class="alt"' : ''; $i++;
+		?>
+		<tbody>
+			<tr<?php echo $class;?>>
+				<td><?php echo anchor('/admin/pages/edit_include/'.$include['includeID'], $include['includeRef']); ?></td>	
+				<td><?php echo dateFmt($include['dateCreated']); ?></td>
+				<td>
+					<?php echo anchor('/admin/pages/edit_include/'.$include['includeID'], 'Edit'); ?>
+				</td>
+				<td>			
+					<?php echo anchor('/admin/pages/delete_include/'.$include['includeID'], 'Delete', 'onclick="return confirm(\'Are you sure you want to delete this?\')"'); ?>
+				</td>
+			</tr>
+		</tbody>
+		<?php endforeach; ?>
+		</table>
+
+		<?php echo $this->pagination->create_links(); ?>
+
+		<?php else: ?>
+
+		<p class="clear">You haven't made any Include files yet.</p>
+
+		<?php endif; ?>
+
+	</div>
 </div>
 
-<div class="clear"></div>
 
-<?php if ($includes): ?>
-
-<?php echo $this->pagination->create_links(); ?>
-
-<table class="default">
-	<tr>
-		<th>Reference</th>
-		<th>Date Modified</th>
-		<th class="tiny">&nbsp;</th>
-		<th class="tiny">&nbsp;</th>
-	</tr>
-<?php
-	$i = 0;
-	foreach ($includes as $include):
-	$class = ($i % 2) ? ' class="alt"' : ''; $i++;
-?>
-	<tr<?php echo $class;?>>
-		<td><?php echo anchor('/admin/pages/edit_include/'.$include['includeID'], $include['includeRef']); ?></td>	
-		<td><?php echo dateFmt($include['dateCreated']); ?></td>
-		<td>
-			<?php echo anchor('/admin/pages/edit_include/'.$include['includeID'], 'Edit'); ?>
-		</td>
-		<td>			
-			<?php echo anchor('/admin/pages/delete_include/'.$include['includeID'], 'Delete', 'onclick="return confirm(\'Are you sure you want to delete this?\')"'); ?>
-		</td>
-	</tr>
-<?php endforeach; ?>
-</table>
-
-<?php echo $this->pagination->create_links(); ?>
-
-<p class="clear" style="text-align: right;"><a href="#" class="button grey" id="totop">Back to top</a></p>
-
-<?php else: ?>
-
-<p class="clear">You haven't made any Include files yet.</p>
-
-<?php endif; ?>
 
