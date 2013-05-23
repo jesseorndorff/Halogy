f34898af8769963689f495e99e1941bd049cdb63
Jesse Orndorff <jesse@jesseorndorff.com>
2013-05-23 11:51:11 +0700

    Fix for fieldreplace.js issue on sites page

--- halogy/modules/halogy/views/sites.php
+++ halogy/modules/halogy/views/sites.php
@@ -1,27 +1,3 @@
-<style type="text/css">
-.ac_results { padding: 0px; border: 1px solid black; background-color: white; overflow: hidden; z-index: 99999; }
-.ac_results ul { width: 100%; list-style-position: outside; list-style: none; padding: 0; margin: 0; }
-.ac_results li { margin: 0px; padding: 2px 5px; cursor: default; display: block; font: menu; font-size: 12px; line-height: 16px; overflow: hidden; }
-.ac_results li span.email { font-size: 10px; } 
-.ac_loading { background: white url('<?php echo $this->config->item('staticPath'); ?>/images/loader.gif') right center no-repeat; }
-.ac_odd { background-color: #eee; }
-.ac_over { background-color: #0A246A; color: white; }
-</style>
-
-<script language="javascript" type="text/javascript" src="<?php echo $this->config->item('staticPath'); ?>/js/jquery.fieldreplace.js"></script>
-<script type="text/javascript">
-$(function(){
-    $('#searchbox').fieldreplace();
-	function formatItem(row) {
-		if (row[0].length) return row[1]+'<br /><span class="email">('+row[0]+')</span>';
-		else return 'No results';
-	}
-	$('#searchbox').autocomplete("<?php echo site_url('/halogy/ac_sites'); ?>", { delay: "0", selectFirst: false, matchContains: true, formatItem: formatItem, minChars: 2 });
-	$('#searchbox').result(function(event, data, formatted){
-		$(this).parent('form').submit();
-	});	
-});
-</script>
 
 <div class="row">
 	<div class="large-12 columns header">
@@ -91,8 +67,6 @@ $(function(){
 		</table>
 
 		<?php echo $this->pagination->create_links(); ?>
-
-		<p style="text-align: right;"><a href="#" class="button grey" id="totop">Back to top</a></p>
 
 		<?php else: ?>
 
