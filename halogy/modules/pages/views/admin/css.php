16ab72f795775d386381f3d117ffd76900ae3315
Jesse Orndorff <jesse@jesseorndorff.com>
2013-05-23 12:39:09 +0700

    update to css page

--- halogy/modules/pages/views/admin/css.php
+++ halogy/modules/pages/views/admin/css.php
@@ -1,51 +1,57 @@
-<h1 class="headingleft">CSS Files</h1>
+<div class="row">
+	<div class="large-12 columns header">
+		<h1 class="headingleft">CSS Files</h1>
 
-<div class="headingright">
-	<a href="<?php echo site_url('/admin/pages/templates'); ?>" class="button blue">Templates</a>
-	<a href="<?php echo site_url('/admin/pages/includes'); ?>" class="button blue">Includes</a>	
-	<a href="<?php echo site_url('/admin/pages/includes/js'); ?>" class="button blue">Javascript</a>	
-	<a href="<?php echo site_url('/admin/pages/add_include/css'); ?>" class="button">Add CSS</a>
+		<ul class="group-button">
+			<li><a href="<?php echo site_url('/admin/pages/templates'); ?>" class="bluebutton">Templates</a></li>
+			<li><a href="<?php echo site_url('/admin/pages/includes'); ?>" class="bluebutton">Includes</a></li>
+			<li><a href="<?php echo site_url('/admin/pages/includes/js'); ?>" class="bluebutton">Javascript</a></li>	
+			<li><a href="<?php echo site_url('/admin/pages/add_include/css'); ?>" class="green">Add CSS</a></li>
+		</ul>
+	</div>
 </div>
 
-<div class="hidden">
-	<p class="hide"><a href="#">x</a></p>
-	<div class="inner"></div>
+<div class="row">
+	<div class="large-12 columns body">
+
+		<?php if ($includes): ?>
+
+		<?php echo $this->pagination->create_links(); ?>
+
+		<table class="default clear">
+			<thead>
+				<tr>
+					<th>Filename</th>
+					<th class="tiny">&nbsp;</th>
+					<th class="tiny">&nbsp;</th>
+				</tr>
+			</thead>
+			<?php
+				$i = 0;
+				foreach ($includes as $include):
+				$class = ($i % 2) ? ' class="alt"' : ''; $i++;
+			?>
+			<tbody>
+				<tr<?php echo $class;?>>
+					<td><?php echo anchor('/admin/pages/edit_include/'.$include['includeID'], $include['includeRef']); ?></td>	
+					<td>
+						<?php echo anchor('/admin/pages/edit_include/'.$include['includeID'], 'Edit'); ?>
+					</td>
+					<td>			
+						<?php echo anchor('/admin/pages/delete_include/'.$include['includeID'].'/css', 'Delete', 'onclick="return confirm(\'Are you sure you want to delete this?\')"'); ?>
+					</td>
+				</tr>
+			</tbody>
+		<?php endforeach; ?>
+		</table>
+
+		<?php echo $this->pagination->create_links(); ?>
+
+		<?php else: ?>
+
+		<p class="clear">You haven't made any CSS files yet.</p>
+
+		<?php endif; ?>
+	</div>
 </div>
-
-<?php if ($includes): ?>
-
-<?php echo $this->pagination->create_links(); ?>
-
-<table class="default clear">
-	<tr>
-		<th>Filename</th>
-		<th class="tiny">&nbsp;</th>
-		<th class="tiny">&nbsp;</th>
-	</tr>
-<?php
-	$i = 0;
-	foreach ($includes as $include):
-	$class = ($i % 2) ? ' class="alt"' : ''; $i++;
-?>
-	<tr<?php echo $class;?>>
-		<td><?php echo anchor('/admin/pages/edit_include/'.$include['includeID'], $include['includeRef']); ?></td>	
-		<td>
-			<?php echo anchor('/admin/pages/edit_include/'.$include['includeID'], 'Edit'); ?>
-		</td>
-		<td>			
-			<?php echo anchor('/admin/pages/delete_include/'.$include['includeID'].'/css', 'Delete', 'onclick="return confirm(\'Are you sure you want to delete this?\')"'); ?>
-		</td>
-	</tr>
-<?php endforeach; ?>
-</table>
-
-<?php echo $this->pagination->create_links(); ?>
-
-<p class="clear" style="text-align: right;"><a href="#" class="button grey" id="totop">Back to top</a></p>
-
-<?php else: ?>
-
-<p class="clear">You haven't made any CSS files yet.</p>
-
-<?php endif; ?>
 
