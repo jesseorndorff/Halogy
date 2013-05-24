cd7b6de7c6c417557e960499405fe96c232cce3a
Jesse Orndorff <jesse@jesseorndorff.com>
2013-05-24 10:24:21 +0700

    Fix for responsive topbar

--- static/js/foundation/foundation.js
+++ static/js/foundation/foundation.js
@@ -13,21 +13,22 @@
 // See http://docs.jquery.com/Using_jQuery_with_Other_Libraries
 // and http://zeptojs.com/
 var libFuncName = null;
+
 if (typeof jQuery === "undefined" &&
     typeof Zepto === "undefined" &&
     typeof $ === "function") {
-    libFuncName = $;
+  libFuncName = $;
 } else if (typeof jQuery === "function") {
-    libFuncName = jQuery;
+  libFuncName = jQuery;
 } else if (typeof Zepto === "function") {
-    libFuncName = Zepto;
+  libFuncName = Zepto;
 } else {
-    throw new TypeError();
+  throw new TypeError();
 }
 
-(function ($) {
+(function ($, window, document, undefined) {
+  'use strict';
 
-(function () {
   // add dusty browser stuff
   if (!Array.prototype.filter) {
     Array.prototype.filter = function(fun /*, thisp */) {
@@ -36,17 +37,13 @@ if (typeof jQuery === "undefined" &&
       if (this == null) {
         throw new TypeError();
       }
-   
+
       var t = Object(this),
           len = t.length >>> 0;
       if (typeof fun != "function") {
-        try {
-          throw new TypeError();
-        } catch (e) {
           return;
-        }
       }
-   
+
       var res = [],
           thisp = arguments[1];
       for (var i = 0; i < len; i++) {
@@ -57,32 +54,65 @@ if (typeof jQuery === "undefined" &&
           }
         }
       }
-   
-      return res;
-    };
 
-    if (!Function.prototype.bind) {
-      Function.prototype.bind = function (oThis) {
-        if (typeof this !== "function") {
-          // closest thing possible to the ECMAScript 5 internal IsCallable function
-          throw new TypeError("Function.prototype.bind - what is trying to be bound is not callable");
+      return res;
+    }
+  }
+
+  if (!Function.prototype.bind) {
+    Function.prototype.bind = function (oThis) {
+      if (typeof this !== "function") {
+        // closest thing possible to the ECMAScript 5 internal IsCallable function
+        throw new TypeError("Function.prototype.bind - what is trying to be bound is not callable");
+      }
+   
+      var aArgs = Array.prototype.slice.call(arguments, 1), 
+          fToBind = this, 
+          fNOP = function () {},
+          fBound = function () {
+            return fToBind.apply(this instanceof fNOP && oThis
+               ? this
+               : oThis,
+             aArgs.concat(Array.prototype.slice.call(arguments)));
+          };
+   
+      fNOP.prototype = this.prototype;
+      fBound.prototype = new fNOP();
+   
+      return fBound;
+    };
+  }
+
+  if (!Array.prototype.indexOf) {
+    Array.prototype.indexOf = function (searchElement /*, fromIndex */ ) {
+      "use strict";
+      if (this == null) {
+        throw new TypeError();
+      }
+      var t = Object(this);
+      var len = t.length >>> 0;
+      if (len === 0) {
+        return -1;
+      }
+      var n = 0;
+      if (arguments.length > 1) {
+        n = Number(arguments[1]);
+        if (n != n) { // shortcut for verifying if it's NaN
+          n = 0;
+        } else if (n != 0 && n != Infinity && n != -Infinity) {
+          n = (n > 0 || -1) * Math.floor(Math.abs(n));
         }
-     
-        var aArgs = Array.prototype.slice.call(arguments, 1), 
-            fToBind = this, 
-            fNOP = function () {},
-            fBound = function () {
-              return fToBind.apply(this instanceof fNOP && oThis
-                 ? this
-                 : oThis,
-               aArgs.concat(Array.prototype.slice.call(arguments)));
-            };
-     
-        fNOP.prototype = this.prototype;
-        fBound.prototype = new fNOP();
-     
-        return fBound;
-      };
+      }
+      if (n >= len) {
+          return -1;
+      }
+      var k = n >= 0 ? n : Math.max(len - Math.abs(n), 0);
+      for (; k < len; k++) {
+        if (k in t && t[k] === searchElement) {
+          return k;
+        }
+      }
+      return -1;
     }
   }
 
@@ -90,17 +120,12 @@ if (typeof jQuery === "undefined" &&
   $.fn.stop = $.fn.stop || function() {
     return this;
   };
-}());
-
-;(function (window, document, undefined) {
-  'use strict';
 
   window.Foundation = {
     name : 'Foundation',
 
-    version : '4.0.0',
+    version : '4.1.5',
 
-    // global Foundation cache object
     cache : {},
 
     init : function (scope, libraries, method, options, response, /* internal */ nc) {
@@ -112,6 +137,9 @@ if (typeof jQuery === "undefined" &&
       // disable library error catching,
       // used for development only
       if (nc) this.nc = nc;
+
+      // check RTL
+      this.rtl = /rtl/i.test($('html').attr('dir'));
 
       // set foundation global scope
       this.scope = scope || this.scope;
@@ -141,9 +169,9 @@ if (typeof jQuery === "undefined" &&
     },
 
     response_obj : function (response_arr, args) {
-      for (var callback in args) {
-        if (typeof args[callback] === 'function') {
-          return args[callback]({
+      for (var i = 0, len = args.length; i < len; i++) {
+        if (typeof args[i] === 'function') {
+          return args[i]({
             errors: response_arr.filter(function (s) {
               if (typeof s === 'string') return s;
             })
@@ -159,6 +187,9 @@ if (typeof jQuery === "undefined" &&
         if (this.libs.hasOwnProperty(lib)) {
           this.patch(this.libs[lib]);
           return this.libs[lib].init.apply(this.libs[lib], args);
+        }
+        else {
+          return function () {};
         }
       }.bind(this), lib);
     },
@@ -177,6 +208,8 @@ if (typeof jQuery === "undefined" &&
 
     patch : function (lib) {
       this.fix_outer(lib);
+      lib.scope = this.scope;
+      lib.rtl = this.rtl;
     },
 
     inherit : function (scope, methods) {
@@ -189,16 +222,31 @@ if (typeof jQuery === "undefined" &&
       }
     },
 
+    random_str : function (length) {
+      var chars = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXTZabcdefghiklmnopqrstuvwxyz'.split('');
+
+      if (!length) {
+          length = Math.floor(Math.random() * chars.length);
+      }
+
+      var str = '';
+      for (var i = 0; i < length; i++) {
+          str += chars[Math.floor(Math.random() * chars.length)];
+      }
+      return str;
+    },
+
     libs : {},
 
     // methods that can be inherited in libraries
     lib_methods : {
       set_data : function (node, data) {
         // this.name references the name of the library calling this method
-        var id = this.name + (+new Date());
+        var id = [this.name,+new Date(),Foundation.random_str(5)].join('-');
 
         Foundation.cache[id] = data;
         node.attr('data-' + this.name + '-id', id);
+        return data;
       },
 
       get_data : function (node) {
@@ -228,7 +276,7 @@ if (typeof jQuery === "undefined" &&
         };
       },
 
-      // parses data-options attribute on page nodes and turns
+      // parses data-options attribute on nodes and turns
       // them into an object
       data_options : function (el) {
         var opts = {}, ii, p,
@@ -236,7 +284,7 @@ if (typeof jQuery === "undefined" &&
             opts_len = opts_arr.length;
 
         function isNumber (o) {
-          return ! isNaN (o-0) && o !== null && o !== "" && o !== false;
+          return ! isNaN (o-0) && o !== null && o !== "" && o !== false && o !== true;
         }
 
         function trim(str) {
@@ -252,7 +300,7 @@ if (typeof jQuery === "undefined" &&
           if (/false/i.test(p[1])) p[1] = false;
           if (isNumber(p[1])) p[1] = parseInt(p[1], 10);
 
-          if (p.length === 2) {
+          if (p.length === 2 && p[0].length > 0) {
             opts[trim(p[0])] = trim(p[1]);
           }
         }
@@ -341,7 +389,7 @@ if (typeof jQuery === "undefined" &&
         return jQuery;
       }
     }()
-  },
+  };
 
   $.fn.foundation = function () {
     var args = Array.prototype.slice.call(arguments, 0);
@@ -352,6 +400,4 @@ if (typeof jQuery === "undefined" &&
     });
   };
 
-}(this, this.document));
-
-})(libFuncName);
+}(libFuncName, this, this.document));
