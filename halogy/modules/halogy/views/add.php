4dc31c023fb6af174ca430539e87a7eeda65e7fb
Jesse Orndorff <jesse@jesseorndorff.com>
2013-06-19 13:15:29 +0700

    disabled custom form for add site

--- halogy/modules/halogy/views/add.php
+++ halogy/modules/halogy/views/add.php
@@ -78,7 +78,7 @@ $(function(){
 });
 </script>
 
-<form method="post" action="<?php echo site_url($this->uri->uri_string()); ?>" class="custom">
+<form method="post" action="<?php echo site_url($this->uri->uri_string()); ?>" class="customDISABLED">
 
 <div class="row">
 	<div class="large-12 columns body">
