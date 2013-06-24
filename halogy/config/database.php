fb5d408929358b1b975db77dd9554d86e0bfbe78
Jesse Orndorff <jesse@jesseorndorff.com>
2013-06-25 06:58:27 +0700

    database

--- halogy/config/database.php
+++ halogy/config/database.php
@@ -59,58 +59,7 @@ $db['default']['autoinit'] = TRUE;
 $db['default']['stricton'] = FALSE;
 
 
-// Setup for Pagoda Box
 
-/* $active_record = TRUE;
- 
-$db['production']['hostname'] = "tunnel.pagodabox.com";
-$db['production']['username'] = "phyllis";
-$db['production']['password'] = "hFbWTODj";
-$db['production']['database'] = "halogy";
-$db['production']['port']     = "3306";
-$db['production']['dbdriver'] = "mysql";
-$db['production']['dbprefix'] = "ha_";
-$db['production']['pconnect'] = TRUE;
-$db['production']['db_debug'] = TRUE;
-$db['production']['cache_on'] = FALSE;
-$db['production']['cachedir'] = "";
-$db['production']['char_set'] = "utf8";
-$db['production']['dbcollat'] = "utf8_general_ci";
-$db['production']['swap_pre'] = '';
-$db['production']['autoinit'] = TRUE;
-$db['production']['stricton'] = FALSE;
-
-// production
-$db['development']['hostname'] = "tunnel.pagodabox.com:3307";
-$db['development']['username'] = "jacalyn";
-$db['development']['password'] = "vH9LgF5f";
-$db['development']['database'] = "halogy-prod";
-$db['development']['dbdriver'] = "mysql";
-$db['development']['dbprefix'] = "ha_";
-$db['development']['pconnect'] = TRUE;
-$db['development']['db_debug'] = TRUE;
-$db['development']['cache_on'] = FALSE;
-$db['development']['cachedir'] = "";
-$db['development']['char_set'] = "utf8";
-$db['development']['dbcollat'] = "utf8_general_ci";
-$db['development']['swap_pre'] = '';
-$db['development']['autoinit'] = TRUE;
-$db['development']['stricton'] = FALSE;
- 
-// Switch active_group based on environment
-switch (ENVIRONMENT) {
-    case 'production':
-        $active_group = 'production';
-    break;
- 
-    // add additional cases for more environments
- 
-    default:
-        $active_group = 'development';
-    break;
-}
-
-*/
 
 
 /* End of file database.php */
