7e5888544127cd9e6a8b6de8cae50ad51679394b
Jesse Orndorff <jesse@jesseorndorff.com>
2014-04-09 20:49:18 +0700

    blog viewall styles

--- halogy/modules/blog/views/admin/viewall.php
+++ halogy/modules/blog/views/admin/viewall.php
@@ -1,43 +1,41 @@
-
-	<div class="large-10 columns body">
-		<h2 class="left">Blog Posts</h2>
-		<ul class="button-group right">
+<div class="large-12 columns body">
+	<div class="card">
+		<div class="header">
+			<div class="small-12 medium-6 large-4 columns left">
+				<h2>Blog Posts</h2>
+			</div>
+			<div class="small-12 medium-5 large-2 columns right">
 			<?php if (in_array('blog_edit', $this->permission->permissions)): ?>
-				<li><a href="<?php echo site_url('/admin/blog/add_post'); ?>" class="button green"><i class="ss-icon">Add</i> Add a Post</a></li>
+				<a href="<?php echo site_url('/admin/blog/add_post'); ?>" class="small button radius secondary">Add a Post</a>
 			<?php endif; ?>
-		</ul>
+			</div>
+		</div>
 
 		<?php if ($blog_posts): ?>
 
 		<?php echo $this->pagination->create_links(); ?>
-		
-		<ul class="small-block-grid-1 large-block-grid-2">
 			<?php foreach ($blog_posts as $post): ?>
-			<li>
-				<div class="card">
-					<p class="posted"><?php echo dateFmt($post['dateCreated'], '', '', TRUE); ?></p>
-					<h3><?php echo (in_array('blog_edit', $this->permission->permissions)) ? anchor('/admin/blog/edit_post/'.$post['postID'], $post['postTitle']) : $post['postTitle']; ?></h3>
-					<p class="excerpt"><?php echo $post['excerpt']; ?></p>
-					<p class="author">Posted By: <!--<?php echo $post['author']; ?>--></p>
-					<div class="card-status">
+				<div class="row pages">
+					<div class="large-4 columns">
+						<strong><?php echo (in_array('blog_edit', $this->permission->permissions)) ? anchor('/admin/blog/edit_post/'.$post['postID'], $post['postTitle']) : $post['postTitle']; ?></strong>
+					</div>
+					<div class="large-5 columns">
 						<?php
 							if ($post['published']) echo '<span class="published">Published</span>';
 							else echo '<span class="not-published">Not Published</span>';
 						?>
+						<p class="posted">Posted On: <?php echo dateFmt($post['dateCreated'], '', '', TRUE); ?></p>
 					</div>
-					<div class="card-admin">
-							<?php if (in_array('blog_edit', $this->permission->permissions)): ?>
-								<?php echo anchor('/admin/blog/edit_post/'.$post['postID'], 'Edit'); ?>
-							<?php endif; ?>
-							<?php if (in_array('blog_delete', $this->permission->permissions)): ?>
-								<?php echo anchor('/admin/blog/delete_post/'.$post['postID'], 'Delete', 'onclick="return confirm(\'Are you sure you want to delete this?\')"'); ?>
-							<?php endif; ?>
+					<div class="small-12 large-3 columns text-right">
+						<?php if (in_array('blog_edit', $this->permission->permissions)): ?>
+							<?php echo anchor('/admin/blog/edit_post/'.$post['postID'], 'Edit', array('class' => 'tiny button grey')); ?>
+						<?php endif; ?>
+						<?php if (in_array('blog_delete', $this->permission->permissions)): ?>
+							<?php echo anchor('/admin/blog/delete_post/'.$post['postID'], 'Delete', array('class' => 'tiny alert button radius', 'onclick' => 'return confirm(\'Are you sure you want to delete this?\')')); ?>
+						<?php endif; ?>
 					</div>
 				</div>
-			</li>
 			<?php endforeach; ?>
-		</ul>
-
 		<?php echo $this->pagination->create_links(); ?>
 
 		<?php else: ?>
