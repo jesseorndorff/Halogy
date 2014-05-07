5db43a45bac0dd5575774830864e0e7310a0a0e6
visasak <visal.visasak@gmail.com>
2014-05-07 15:04:44 +0700

    Modify /blog/comments match style of the rest of the site

--- halogy/modules/blog/views/admin/comments.php
+++ halogy/modules/blog/views/admin/comments.php
@@ -1,6 +1,13 @@
-			<div class="large-10 columns body">
+<div class="large-12 columns body">
+	<div class="card">
+		<div class="header">
+			<div class="small-12 medium-6 large-4 columns left">
 				<h2>Blog Comments</h2>
-
+			</div>
+			<div class="large-6 small-12 columns right"></div>
+		</div>
+		<div class="row table">
+			<div class="small-12 columns item">
 				<?php if ($comments): ?>
 
 				<?php echo $this->pagination->create_links(); ?>
@@ -25,37 +32,6 @@
 					<?php endforeach; ?>
 				</ul>
 
-				<!-- <table class="default clear">
-					<thead>
-						<tr>
-							<th>Date Posted</th>
-							<th>Post</th>
-							<th>Author</th>
-							<th>Email</th>
-							<th>Comment</th>	
-							<th>Status</th>
-							<th class="tiny">&nbsp;</th>
-							<th class="tiny">&nbsp;</th>
-						</tr>
-						<tbody>
-							<?php foreach ($comments as $comment): ?>
-								<tr>
-									<td><?php echo dateFmt($comment['dateCreated']); ?></td>
-							
-									<td><?php echo $comment['fullName']; ?></td>
-									<td><?php echo $comment['email']; ?></td>
-									<td><?php echo (strlen($comment['comment'] > 50)) ? htmlentities(substr($comment['comment'], 0, 50)).'...' : htmlentities($comment['comment']); ?></td>						
-									<td><?php echo ($comment['active']) ? '<span style="color:green;">Active</span>' : '<span style="color:orange;">Pending</span>'; ?></td>		
-									<td><?php echo (!$comment['active']) ? anchor('/admin/blog/approve_comment/'.$comment['commentID'], 'Approve') : ''; ?></td>
-									<td>
-										<?php echo anchor('/admin/blog/delete_comment/'.$comment['commentID'], 'Delete', 'onclick="return confirm(\'Are you sure you want to delete this?\')"'); ?>
-									</td>
-								</tr>
-							<?php endforeach; ?>
-						</tbody>
-					</thead>
-				</table> -->
-
 				<?php echo $this->pagination->create_links(); ?>
 
 				<?php else: ?>
@@ -63,8 +39,7 @@
 				<p class="clear">There are no comments yet.</p>
 
 				<?php endif; ?>
-			</div> <!-- /10 -->
+			</div>
 		</div>
 	</div>
 </div>
-
