beaf407ca4c3a3f9331ccf9d232bc5d3a81740af
visasak <visal.visasak@gmail.com>
2014-05-07 13:43:53 +0700

    Modify /webforms/tickets match style of the rest of the site

--- halogy/modules/webforms/views/tickets.php
+++ halogy/modules/webforms/views/tickets.php
@@ -6,87 +6,87 @@ $(function(){
 	});	
 });
 </script>
-
-	<div class="large-12 columns body">
-		<h2 class="left">Tickets <?php if ($status) echo ' - ('.$status.')'?> </h2>
-		<div class="right">
-			<a href="<?php echo site_url('/admin/webforms/viewall'); ?>" class="button">View Web Forms</a>
-		</div>
-		<div class="clear"></div>
-		<div class="large-4 columns push-8">
-			<label for="filter">
-				Filter
-			</label>
-
-			<?php
-				$options[''] = 'View All';
-				$options['open'] = 'Open';
-				$options['closed'] = 'Closed';
-
-				$options['-'] = '--------------------';
-
-				if ($webforms)
-				{
-					foreach($webforms as $form)
-					{
-						$options[$form['formID']] = $form['formName'];
-					}
-				}
-				
-				echo form_dropdown('filter', $options, $this->uri->segment(4), 'id="filter"');
-			?>
-		</div>
-
-		<?php if ($tickets): ?>
-
-		<?php echo $this->pagination->create_links(); ?>
-
-		<table class="default">
-			<thead>
-				<tr>
-					<th><?php echo order_link('admin/webforms/tickets','subject','Subject'); ?></th>
-					<th><?php echo order_link('admin/webforms/tickets','dateCreated','Date'); ?></th>
-					<th><?php echo order_link('admin/webforms/tickets','formName','Web Form'); ?></th>		
-					<th><?php echo order_link('admin/webforms/tickets','status','Status'); ?></th>		
-					<th><?php echo order_link('admin/webforms/tickets','fullName','Name'); ?></th>
-					<th><?php echo order_link('admin/webforms/tickets','email','Email'); ?></th>
-					<th class="tiny">&nbsp;</th>
-					<th class="tiny">&nbsp;</th>
-				</tr>
-			</thead>
-			<tbody>
+<div class="large-12 columns body">
+	<div class="card">
+		<div class="header">
+			<div class="small-12 medium-6 large-4 columns left">
+				<h2>Tickets <?php if ($status) echo ' - ('.$status.')'?> </h2>
+				<label for="filter">Filter</label>
 				<?php
-					$i=0;
-					foreach ($tickets as $ticket):
-					$class = ($i % 2) ? ' class="alt"' : ''; 
-					$style = (!$ticket['viewed']) ? ' style="font-weight: bold;"' : '';
-					$i++;
+					$options[''] = 'View All';
+					$options['open'] = 'Open';
+					$options['closed'] = 'Closed';
+					$options['-'] = '--------------------';
+
+					if ($webforms)
+					{
+						foreach($webforms as $form)
+						{
+							$options[$form['formID']] = $form['formName'];
+						}
+					}
+					echo form_dropdown('filter', $options, $this->uri->segment(4), 'id="filter"');
 				?>
-					<tr<?php echo $class; ?><?php echo $style; ?>>
-						<td><?php echo anchor('/admin/webforms/view_ticket/'.$ticket['ticketID'], '[#'.$ticket['ticketID'].']: '.$ticket['subject']); ?></td>	
-						<td><?php echo dateFmt($ticket['dateCreated'], '', '', TRUE); ?></td>
-						<td><?php echo ($ticket['formName']) ? anchor('/admin/webforms/viewall', $ticket['formName']) : ''; ?></td>
-						<td><?php echo ($ticket['closed']) ? 'Closed' : 'Open'; ?></td>
-						<td><?php echo $ticket['fullName']; ?></td>
-						<td><?php echo $ticket['email']; ?></td>		
-						<td class="tiny">
-							<?php echo anchor('/admin/webforms/view_ticket/'.$ticket['ticketID'], 'Edit'); ?>
-						</td>
-						<td class="tiny">
-							<?php if (in_array('webforms_tickets', $this->permission->permissions)): ?>	
-								<?php echo anchor('/admin/webforms/delete_ticket/'.$ticket['ticketID'], 'Delete', 'onclick="return confirm(\'Are you sure you want to delete this?\')"'); ?>
-							<?php endif; ?>
-						</td>
-					</tr>
-				<?php endforeach; ?>
-		</tbody>
-		</table>
+			</div>
+			<div class="large-6 small-12 columns right">
+				<a href="<?php echo site_url('/admin/webforms/viewall'); ?>" class="button small radius">View Web Forms</a>
+			</div>
+		</div>
+		<div class="row table">
+			<div class="small-12 columns item">
+				<?php if ($tickets): ?>
 
-		<?php echo $this->pagination->create_links(); ?>
+				<?php echo $this->pagination->create_links(); ?>
 
-		<?php else: ?>
+				<table class="default">
+					<thead>
+						<tr>
+							<th><?php echo order_link('admin/webforms/tickets','subject','Subject'); ?></th>
+							<th><?php echo order_link('admin/webforms/tickets','dateCreated','Date'); ?></th>
+							<th><?php echo order_link('admin/webforms/tickets','formName','Web Form'); ?></th>		
+							<th><?php echo order_link('admin/webforms/tickets','status','Status'); ?></th>		
+							<th><?php echo order_link('admin/webforms/tickets','fullName','Name'); ?></th>
+							<th><?php echo order_link('admin/webforms/tickets','email','Email'); ?></th>
+							<th class="tiny">&nbsp;</th>
+							<th class="tiny">&nbsp;</th>
+						</tr>
+					</thead>
+					<tbody>
+						<?php
+							$i=0;
+							foreach ($tickets as $ticket):
+							$class = ($i % 2) ? ' class="alt"' : ''; 
+							$style = (!$ticket['viewed']) ? ' style="font-weight: bold;"' : '';
+							$i++;
+						?>
+							<tr <?php echo $class; ?><?php echo $style; ?>>
+								<td><?php echo anchor('/admin/webforms/view_ticket/'.$ticket['ticketID'], '[#'.$ticket['ticketID'].']: '.$ticket['subject']); ?></td>	
+								<td><?php echo dateFmt($ticket['dateCreated'], '', '', TRUE); ?></td>
+								<td><?php echo ($ticket['formName']) ? anchor('/admin/webforms/viewall', $ticket['formName']) : ''; ?></td>
+								<td><?php echo ($ticket['closed']) ? 'Closed' : 'Open'; ?></td>
+								<td><?php echo $ticket['fullName']; ?></td>
+								<td><?php echo $ticket['email']; ?></td>		
+								<td class="tiny">
+									<?php echo anchor('/admin/webforms/view_ticket/'.$ticket['ticketID'], 'Edit'); ?>
+								</td>
+								<td class="tiny">
+									<?php if (in_array('webforms_tickets', $this->permission->permissions)): ?>	
+										<?php echo anchor('/admin/webforms/delete_ticket/'.$ticket['ticketID'], 'Delete', 'onclick="return confirm(\'Are you sure you want to delete this?\')"'); ?>
+									<?php endif; ?>
+								</td>
+							</tr>
+						<?php endforeach; ?>
+					</tbody>
+				</table>
 
-		<p class="clear">There are no tickets here yet.</p>
+				<?php echo $this->pagination->create_links(); ?>
 
-		<?php endif; ?>
-	</div> <!-- /10 -->
+				<?php else: ?>
+
+				<p class="clear">There are no tickets here yet.</p>
+
+				<?php endif; ?>
+			</div>
+		</div>
+	</div>
+</div>
