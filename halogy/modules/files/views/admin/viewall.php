1cc947e2b9d2f70db53ec4bdf5409e9507f2ea78
visasak <visal.visasak@gmail.com>
2014-05-07 10:46:45 +0700

    Modify /files/viewall match style of the rest of the site

--- halogy/modules/files/views/admin/viewall.php
+++ halogy/modules/files/views/admin/viewall.php
@@ -50,16 +50,9 @@ $(function(){
 </script>
 <div class="large-12 columns body">
 	<div class="card">
-		<h2 class="left">Files</h2>
-		<div class="right">
-			<?php if ($this->site->config['plan'] = 0 || $this->site->config['plan'] = 6 || (($this->site->config['plan'] > 0 && $this->site->config['plan'] < 6) && $quota < $this->site->plans['storage'])): ?>
-				<a href="#" class="button green toggle" data-reveal-id="upload-file">Upload File</a>
-			<?php endif; ?>
-		</div>
-
-		<div class="clear"></div>
-
-			<div class="large-4 columns">
+		<div class="header">
+			<div class="small-12 medium-6 large-4 columns left">
+				<h2>Files</h2>
 				<label for="folderID">
 					Folder
 				</label> 
@@ -79,109 +72,103 @@ $(function(){
 					echo form_dropdown('folderID', $options, $folderID, 'id="folderID"');
 				?>
 			</div>
-			<!-- <div class="large-4 large-offset-4 columns">
-				<div class="row collapse">
-					<form method="post" action="<?php echo site_url('/admin/files/viewall'); ?>" class="custom" id="search">
-						<div class="small-9 columns">
-							<input type="text" name="searchbox" id="searchbox" class="formelement inactive" placeholder="Search Files..." />
-						</div>
-						<div class="small-3 columns">
-							<input type="submit" class="button prefix" id="searchbutton" value="Search" />
-						</div>
-					</form>
-				</div>
-			</div> -->
-
-			<?php if ($errors = validation_errors()): ?>
-				<div class="error clear">
-					<?php echo $errors; ?>
-				</div>
-			<?php endif; ?>
-
-
-			<div id="loader" class="hidden clear"></div>
-
-			<?php if ($this->site->config['plan'] > 0 && $this->site->config['plan'] < 6): ?>
-
-				<?php if ($quota > $this->site->plans['storage']): ?>
-				
-				<div class="error clear">
-					<p>You have gone over your storage capacity, we will be contacting you soon.</p>
-				</div>
-				
-				<div class="quota">
-					<div class="over"><?php echo floor($quota / $this->site->plans['storage'] * 100); ?>%</div>
-				</div>
-				
-				<?php else: ?>
-				
-				<div class="quota">
-					<div class="used" style="width: <?php echo ($quota > 0) ? (floor($quota / $this->site->plans['storage'] * 100)) : 0; ?>%"><?php echo floor($quota / $this->site->plans['storage'] * 100); ?>%</div>
-				</div>
-				
+			<div class="large-6 small-12 columns right">
+				<?php if ($this->site->config['plan'] = 0 || $this->site->config['plan'] = 6 || (($this->site->config['plan'] > 0 && $this->site->config['plan'] < 6) && $quota < $this->site->plans['storage'])): ?>
+					<a href="#" class="button small radius green toggle" data-reveal-id="upload-file">Upload File</a>
+				<?php endif; ?>
+			</div>
+		</div>
+		<div class="row table">
+			<div class="small-12 columns">
+				<?php if ($errors = validation_errors()): ?>
+					<div class="error clear">
+						<?php echo $errors; ?>
+					</div>
 				<?php endif; ?>
 
-				<p><small>You have used <strong><?php echo number_format($quota); ?>kb</strong> out of your <strong><?php echo number_format($this->site->plans['storage']); ?> KB</strong> quota.</small></p>
+				<div id="loader" class="hidden clear"></div>
 
-			<?php endif; ?>
+				<?php if ($this->site->config['plan'] > 0 && $this->site->config['plan'] < 6): ?>
 
-			<?php if ($files): ?>
-
-
-				<?php echo $this->pagination->create_links(); ?>
-				
-			<ul class="small-block-grid-1 large-block-grid-4">
-					<?php
-						$numItems = sizeof($files);
-						$itemsPerRow = 6;
-						$i = 0;
-									
-						foreach ($files as $file)
-						{
-							if (($i % $itemsPerRow) == 0 && $i > 1)
-							{
-								echo '</tr><tr>'."\n";
-								$i = 0;
-							}
-							echo '<td align="center" valign="top" width="'.floor(( 1 / $itemsPerRow) * 100).'%">';
-
-							$extension = substr($file['filename'], strpos($file['filename'], '.')+1);
-							$filePath = '/files/'.$file['fileRef'].'.'.$extension;				
-
-					?>
-
-							<li>
-								<div class="card">
-									<h3><?php echo $file['fileRef']; ?></h3>
-									<a href="<?php echo $filePath; ?>" title="<?php echo $file['fileRef']; ?>"><img src="<?php echo $this->config->item('staticPath'); ?>/fileicons/<?php echo $extension; ?>.png" alt="<?php echo $file['fileRef']; ?>" class="file" /></a>
-									<div class="card-admin">
-										<?php echo anchor('/admin/files/edit/'.$file['fileID'].'/', 'Edit'); ?>
-										<?php echo anchor('/admin/files/delete/'.$file['fileID'], 'Delete', 'onclick="return confirm(\'Are you sure you want to delete this file?\')"'); ?>
-									</div>
-								</div>
-							</li>
-						
-					<?php
-							echo '</td>'."\n";
-							$i++;
-						}
+					<?php if ($quota > $this->site->plans['storage']): ?>
 					
-						for($x = 0; $x < ($itemsPerRow - $i); $x++)
-						{
-							echo '<td width="'.floor((1 / $itemsPerRow) * 100).'%">&nbsp;</td>';
-						}
-					?>
-			</ul>
-				
-				<?php echo $this->pagination->create_links(); ?>
+					<div class="error clear">
+						<p>You have gone over your storage capacity, we will be contacting you soon.</p>
+					</div>
+					
+					<div class="quota">
+						<div class="over"><?php echo floor($quota / $this->site->plans['storage'] * 100); ?>%</div>
+					</div>
+					
+					<?php else: ?>
+					
+					<div class="quota">
+						<div class="used" style="width: <?php echo ($quota > 0) ? (floor($quota / $this->site->plans['storage'] * 100)) : 0; ?>%"><?php echo floor($quota / $this->site->plans['storage'] * 100); ?>%</div>
+					</div>
+					
+					<?php endif; ?>
+
+					<p><small>You have used <strong><?php echo number_format($quota); ?>kb</strong> out of your <strong><?php echo number_format($this->site->plans['storage']); ?> KB</strong> quota.</small></p>
+
+				<?php endif; ?>
+
+				<?php if ($files): ?>
 
 
-			<?php else: ?>
+					<?php echo $this->pagination->create_links(); ?>
+					
+				<ul class="small-block-grid-1 large-block-grid-4">
+						<?php
+							$numItems = sizeof($files);
+							$itemsPerRow = 6;
+							$i = 0;
+										
+							foreach ($files as $file)
+							{
+								if (($i % $itemsPerRow) == 0 && $i > 1)
+								{
+									echo '</tr><tr>'."\n";
+									$i = 0;
+								}
+								echo '<td align="center" valign="top" width="'.floor(( 1 / $itemsPerRow) * 100).'%">';
 
-			<p class="clear">You have not yet uploaded any files.</p>
+								$extension = substr($file['filename'], strpos($file['filename'], '.')+1);
+								$filePath = '/files/'.$file['fileRef'].'.'.$extension;				
 
-			<?php endif; ?>
+						?>
 
+								<li>
+									<div class="card">
+										<h3><?php echo $file['fileRef']; ?></h3>
+										<a href="<?php echo $filePath; ?>" title="<?php echo $file['fileRef']; ?>"><img src="<?php echo $this->config->item('staticPath'); ?>/fileicons/<?php echo $extension; ?>.png" alt="<?php echo $file['fileRef']; ?>" class="file" /></a>
+										<div class="card-admin">
+											<?php echo anchor('/admin/files/edit/'.$file['fileID'].'/', 'Edit'); ?>
+											<?php echo anchor('/admin/files/delete/'.$file['fileID'], 'Delete', 'onclick="return confirm(\'Are you sure you want to delete this file?\')"'); ?>
+										</div>
+									</div>
+								</li>
+							
+						<?php
+								echo '</td>'."\n";
+								$i++;
+							}
+						
+							for($x = 0; $x < ($itemsPerRow - $i); $x++)
+							{
+								echo '<td width="'.floor((1 / $itemsPerRow) * 100).'%">&nbsp;</td>';
+							}
+						?>
+				</ul>
+					
+					<?php echo $this->pagination->create_links(); ?>
+
+
+				<?php else: ?>
+
+				<p class="clear">You have not yet uploaded any files.</p>
+
+				<?php endif; ?>
+			</div>
 		</div>
 	</div>
 </div>
@@ -210,8 +197,8 @@ $(function(){
 		?>	
 		<br class="clear" /><br />
 			
-		<input type="submit" value="Upload File" class="button nolabel" id="submit" />
-		<a href="<?php echo site_url('/admin/files'); ?>" class="button cancel grey">Cancel</a>
+		<input type="submit" value="Upload File" class="button small radius nolabel" id="submit" />
+		<a href="<?php echo site_url('/admin/files'); ?>" class="button small radius cancel grey">Cancel</a>
 		
 	</form>
 	<a class="close-reveal-modal">&#215;</a>
