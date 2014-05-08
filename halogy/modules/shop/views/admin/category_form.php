58d6096495ed364fe41730895b671b75609d1464
visasak <visal.visasak@gmail.com>
2014-05-08 15:49:49 +0700

    Modify /shop/add_cat match style of the rest of the site

--- halogy/modules/shop/views/admin/category_form.php
+++ halogy/modules/shop/views/admin/category_form.php
@@ -1,48 +1,48 @@
-<div class="large-10 columns body">
-	<div class="small-12 large-8 large-centered columns card">
-		<form method="post" action="<?php echo site_url($this->uri->uri_string()); ?>" class="">
-		
-			<?php if (!$this->core->is_ajax()): ?>
-				<h2 class="left"><?php echo (preg_match('/edit/i', $this->uri->segment(3))) ? 'Edit' : 'Add'; ?> Categories</h2>
-			<?php endif; ?>
-
-			<div class="right">
-				<a href="<?php echo site_url('/admin/shop/categories'); ?>" class="button">Back to Categories</a>
-				<input type="submit" value="Save Changes" class="green button nolabel" />
-			</div>
-
-			<div class="clear"></div>
-
-			<?php if ($errors = validation_errors()): ?>
-				<div class="error">
-					<?php echo $errors; ?>
+<form method="post" action="<?php echo site_url($this->uri->uri_string()); ?>" class="">
+	<div class="large-12 columns body">
+		<div class="card">
+			<div class="header">
+				<div class="small-12 medium-6 large-4 columns left">
+					<?php if (!$this->core->is_ajax()): ?>
+						<h2><?php echo (preg_match('/edit/i', $this->uri->segment(3))) ? 'Edit' : 'Add'; ?> Categories</h2>
+					<?php endif; ?>
+					<a href="<?php echo site_url('/admin/shop/categories'); ?>">Back to Categories</a>
 				</div>
-			<?php endif; ?>
-
-			<div class="item">
-				<label for="catName">Title:</label>
-				<?php echo @form_input('catName', $data['catName'], 'class="formelement" id="catName"'); ?>
+				<div class="large-6 small-12 columns right">
+					<input type="submit" value="Save Changes" class="button small radius success nolabel" />
+				</div>
 			</div>
-				
-			<div class="item">
-				<label for="templateID">Parent:</label>
-				<?php
-					$options = '';		
-					$options[0] = 'Top Level';
-					if ($parents):	
-						foreach ($parents as $parent):
-							if ($parent['catID'] != @$data['catID']) $options[$parent['catID']] = $parent['catName'];
-						endforeach;
-					endif;
-					
-					echo @form_dropdown('parentID',$options,$data['parentID'],'id="parentID" class="formelement"');
-				?>
+			<div class="row table">				
+				<div class="small-12 columns">
+					<?php if ($errors = validation_errors()): ?>
+						<div class="error">
+							<?php echo $errors; ?>
+						</div>
+					<?php endif; ?>
+				</div>
+				<div class="small-12 columns item">
+					<label for="catName">Title:</label>
+					<?php echo @form_input('catName', $data['catName'], 'class="formelement" id="catName"'); ?>
+				</div>
+				<div class="small-12 columns item">
+					<label for="templateID">Parent:</label>
+					<?php
+						$options = '';		
+						$options[0] = 'Top Level';
+						if ($parents):	
+							foreach ($parents as $parent):
+								if ($parent['catID'] != @$data['catID']) $options[$parent['catID']] = $parent['catName'];
+							endforeach;
+						endif;
+						
+						echo @form_dropdown('parentID',$options,$data['parentID'],'id="parentID" class="formelement"');
+					?>
+				</div>
+				<div class="small-12 columns item">
+					<label for="description">Description:</label>
+					<?php echo @form_textarea('description',set_value('description', $data['description']), 'id="description" class="formelement short"'); ?>
+				</div>
 			</div>
-			
-			<div class="item">
-				<label for="description">Description:</label>
-				<?php echo @form_textarea('description',set_value('description', $data['description']), 'id="description" class="formelement short"'); ?>
-			</div>
-		</form>
+		</div>
 	</div>
-</div>
+</form>
