2827abcbd6fb0c44d1a322e4b104f9e710bb0235
visasak <visal.visasak@gmail.com>
2014-05-09 10:06:23 +0700

    Modify /shop/postages match style of the rest of the site

--- halogy/modules/shop/views/admin/postages.php
+++ halogy/modules/shop/views/admin/postages.php
@@ -4,52 +4,53 @@ $(function(){
 	$.listen('click', 'input#cancel', function(event){hideForm(this,event);});
 });
 </script>
-
-<div class="large-10 columns body">
+<div class="large-12 columns body">
 	<div class="card">
-		<h2 class="left">Shipping Costs</h2>
-
-		<div class="right">	
-			<a href="<?php echo site_url('/admin/shop/add_postage'); ?>" class="showform green button">Add Shipping Rate</a>
-		</div>
-
-		<div class="clear"></div>
-
-		<div class="hidden"></div>
-
-		<?php if ($shop_postages): ?>
-
-		<div class="row table-header hide-for-touch">
-			<div class="large-5 columns">
-				<h3>Total</h3>
+		<div class="header">
+			<div class="small-12 medium-6 large-4 columns left">
+				<h2>Shipping Costs</h2>
 			</div>
-			<div class="large-5 columns">
-				<h3>Cost</h3>
-			</div>
-			<div class="large-2 columns">
+			<div class="large-6 small-12 columns right">
+				<a href="<?php echo site_url('/admin/shop/add_postage'); ?>" class="button small radius success showform">Add Shipping Rate</a>
 			</div>
 		</div>
-		<?php foreach($shop_postages as $postage): ?>
 		<div class="row table">
-			<div class="large-5 columns">
-				<p><?php echo currency_symbol(); ?><?php echo number_format($postage['total'], 2); ?></p>
-			</div>
-			<div class="large-5 columns">
-				<p><?php echo currency_symbol(); ?><?php echo number_format($postage['cost'], 2); ?></p>
-			</div>
-			<div class="large-2 columns">
-				<ul class="button-group even-2">
-					<li><?php echo anchor('/admin/shop/edit_postage/'.$postage['postageID'], 'Edit', 'class="showform"'); ?></li>
-					<li><?php echo anchor('/admin/shop/delete_postage/'.$postage['postageID'], 'Delete', 'onclick="return confirm(\'Are you sure you want to delete this?\');"'); ?></li>
-				</ul>
+			<div class="small-12 columns item">
+				<?php if ($shop_postages): ?>
+
+				<div class="row table-header hide-for-touch">
+					<div class="large-5 columns">
+						<h3>Total</h3>
+					</div>
+					<div class="large-5 columns">
+						<h3>Cost</h3>
+					</div>
+					<div class="large-2 columns">
+					</div>
+				</div>
+				<?php foreach($shop_postages as $postage): ?>
+				<div class="row table">
+					<div class="large-5 columns">
+						<p><?php echo currency_symbol(); ?><?php echo number_format($postage['total'], 2); ?></p>
+					</div>
+					<div class="large-5 columns">
+						<p><?php echo currency_symbol(); ?><?php echo number_format($postage['cost'], 2); ?></p>
+					</div>
+					<div class="large-2 columns">
+						<ul class="button-group even-2">
+							<li><?php echo anchor('/admin/shop/edit_postage/'.$postage['postageID'], 'Edit', 'class="showform"'); ?></li>
+							<li><?php echo anchor('/admin/shop/delete_postage/'.$postage['postageID'], 'Delete', 'onclick="return confirm(\'Are you sure you want to delete this?\');"'); ?></li>
+						</ul>
+					</div>
+				</div>
+				<?php endforeach; ?>
+
+				<?php else: ?>
+
+				<p>You have not yet set up your shipping costs yet.</p>
+
+				<?php endif; ?>
 			</div>
 		</div>
-		<?php endforeach; ?>
-
-		<?php else: ?>
-
-		<p>You have not yet set up your shipping costs yet.</p>
-
-		<?php endif; ?>
 	</div>
 </div>
