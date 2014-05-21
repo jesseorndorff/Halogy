a64a9c28ccaf820486bdd62ef1fa0d198a884ca3
visasak <visal.visasak@gmail.com>
2014-05-21 15:02:36 +0700

    Modify /shop/reviews match style of the rest of the site

--- halogy/modules/shop/views/admin/reviews.php
+++ halogy/modules/shop/views/admin/reviews.php
@@ -1,66 +1,75 @@
-<div class="large-10 columns body">
+<div class="large-12 columns body">
 	<div class="card">
-		<h2 class="left">Shop Reviews</h2>
-
-		<?php if ($reviews): ?>
-
-		<?php echo $this->pagination->create_links(); ?>
-
-		<div class="row table-header hide-for-touch">
-			<div class="large-2 columns">
-				<h3>Date Posted</h3>
+		<div class="header">
+			<div class="small-12 medium-6 large-4 columns left">
+				<h2>Shop Reviews</h2>
 			</div>
-			<div class="large-1 columns">
-				<h3>Product</h3>
-			</div>
-			<div class="large-2 columns">
-				<h3>Author</h3>
-			</div>
-			<div class="large-2 columns">
-				<h3>Email</h3>
-			</div>
-			<div class="large-2 columns">
-				<h3>Review</h3>
-			</div>
-			<div class="large-1 columns">
-				<h3>Status</h3>
-			</div>
-			<div class="large-2 columns">
+			<div class="large-6 small-12 columns right">
+				
 			</div>
 		</div>
-
-		<?php foreach ($reviews as $review): ?>
 		<div class="row table">
-			<div class="large-2 columns">
-				<p><?php echo dateFmt($review['dateCreated']); ?></p>
-			</div>
-			<div class="large-1 columns">
-				<h3>?php echo anchor('/shop/viewproduct/'.$review['productID'], $review['productName']); ?></h3>
-			</div>
-			<div class="large-2 columns">
-				<h3><?php echo $review['fullName']; ?></h3>
-			</div>
-			<div class="large-2 columns">
-				<h3><?php echo $review['email']; ?></h3>
-			</div>
-			<div class="large-2 columns">
-				<h3><?php echo (strlen($review['review'] > 50)) ? substr($review['review'], 0, 50).'...' : $review['review']; ?></h3>
-			</div>
-			<div class="large-1 columns">
-				<h3><?php echo ($review['active']) ? '<span style="color:green;">Active</span>' : '<span style="color:orange;">Pending</span>'; ?></h3>
-			</div>
-			<div class="large-2 columns">
-				<ul class="button-group even-2">
-					<li><?php echo (!$review['active']) ? anchor('/admin/shop/approve_review/'.$review['reviewID'], 'Approve') : ''; ?></li>
-					<li><?php echo anchor('/admin/shop/delete_review/'.$review['reviewID'], 'Delete', 'onclick="return confirm(\'Are you sure you want to delete this?\')"'); ?></li>
-				</ul>
+			<div class="small-12 columns item">
+				<?php if ($reviews): ?>
+
+				<?php echo $this->pagination->create_links(); ?>
+
+				<div class="row table-header hide-for-touch">
+					<div class="large-2 columns">
+						<h3>Date Posted</h3>
+					</div>
+					<div class="large-1 columns">
+						<h3>Product</h3>
+					</div>
+					<div class="large-2 columns">
+						<h3>Author</h3>
+					</div>
+					<div class="large-2 columns">
+						<h3>Email</h3>
+					</div>
+					<div class="large-2 columns">
+						<h3>Review</h3>
+					</div>
+					<div class="large-1 columns">
+						<h3>Status</h3>
+					</div>
+					<div class="large-2 columns">
+					</div>
+				</div>
+
+				<?php foreach ($reviews as $review): ?>
+				<div class="row table">
+					<div class="large-2 columns">
+						<p><?php echo dateFmt($review['dateCreated']); ?></p>
+					</div>
+					<div class="large-1 columns">
+						<h3>?php echo anchor('/shop/viewproduct/'.$review['productID'], $review['productName']); ?></h3>
+					</div>
+					<div class="large-2 columns">
+						<h3><?php echo $review['fullName']; ?></h3>
+					</div>
+					<div class="large-2 columns">
+						<h3><?php echo $review['email']; ?></h3>
+					</div>
+					<div class="large-2 columns">
+						<h3><?php echo (strlen($review['review'] > 50)) ? substr($review['review'], 0, 50).'...' : $review['review']; ?></h3>
+					</div>
+					<div class="large-1 columns">
+						<h3><?php echo ($review['active']) ? '<span style="color:green;">Active</span>' : '<span style="color:orange;">Pending</span>'; ?></h3>
+					</div>
+					<div class="large-2 columns">
+						<ul class="button-group even-2">
+							<li><?php echo (!$review['active']) ? anchor('/admin/shop/approve_review/'.$review['reviewID'], 'Approve') : ''; ?></li>
+							<li><?php echo anchor('/admin/shop/delete_review/'.$review['reviewID'], 'Delete', 'onclick="return confirm(\'Are you sure you want to delete this?\')"'); ?></li>
+						</ul>
+					</div>
+				</div>
+				<?php endforeach; ?>
+
+				<?php echo $this->pagination->create_links(); ?>
+
+				<?php endif; ?>
 			</div>
 		</div>
-		<?php endforeach; ?>
-
-		<?php echo $this->pagination->create_links(); ?>
-
-		<?php endif; ?>
 	</div>
 </div>
-
