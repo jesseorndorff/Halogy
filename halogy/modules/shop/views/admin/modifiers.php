b2090b3abd37bb5d040ad831009a50cd19e97da3
visasak <visal.visasak@gmail.com>
2014-05-21 10:55:42 +0700

    Modify /shop/modifiers match style of the rest of the site

--- halogy/modules/shop/views/admin/modifiers.php
+++ halogy/modules/shop/views/admin/modifiers.php
@@ -4,57 +4,58 @@ $(function(){
 	$.listen('click', 'input#cancel', function(event){hideForm(this,event);});
 });
 </script>
-
-<div class="large-10 columns body">
+<div class="large-12 columns body">
 	<div class="card">
-		<h2 class="left">Shipping Modifiers</h2>
-
-		<div class="right">	
-			<a href="<?php echo site_url('/admin/shop/add_modifier'); ?>" class="showform green">Add Modifier</a>
+		<div class="header">
+			<div class="small-12 medium-6 large-4 columns left">
+				<h2>Shipping Modifiers</h2>
+			</div>
+			<div class="large-6 small-12 columns right">
+				<a href="<?php echo site_url('/admin/shop/add_modifier'); ?>" class="button small radius showform">Add Modifier</a>
+			</div>
 		</div>
+		<div class="row table">
+			<div class="small-12 columns item">
+				<?php if ($shop_modifiers): ?>
+				<div class="row table-header hide-for-touch">
+					<div class="large-2 columns">
+						<h3>Multiplier</h3>
+					</div>
+					<div class="large-4 columns">
+						<h3>Name</h3>
+					</div>
+					<div class="large-4 columns">
+						<h3>Band</h3>
+					</div>
+					<div class="large-2 columns">
+					</div>			
+				</div>
+					<?php foreach($shop_modifiers as $modifier): ?>
+					<div class="row table">
+						<div class="large-2 columns">
+							<p><?php echo $modifier['multiplier']; ?> x</p>
+						</div>
+						<div class="large-4 columns">
+							<p><?php echo $modifier['modifierName']; ?></p>
+						</div>
+						<div class="large-4 columns">
+							<p><?php echo $modifier['bandName']; ?></p>
+						</div>
+						<div class="large-2 columns">
+							<ul class="button-group even-2">
+								<li><?php echo anchor('/admin/shop/edit_modifier/'.$modifier['modifierID'], 'Edit', 'class="showform"'); ?></li>
+								<li><?php echo anchor('/admin/shop/delete_modifier/'.$modifier['modifierID'], 'Delete', 'onclick="return confirm(\'Are you sure you want to delete this?\');"'); ?></li>
+							</ul>
+						</div>
+					</div>
+					<?php endforeach; ?>
 
-		<div class="clear"></div>
+				<?php else: ?>
 
-		<div class="hidden"></div>
+				<p>You have not yet set up any shipping modifiers yet.</p>
 
-		<?php if ($shop_modifiers): ?>
-		<div class="row table-header hide-for-touch">
-			<div class="large-2 columns">
-				<h3>Multiplier</h3>
+				<?php endif; ?>
 			</div>
-			<div class="large-4 columns">
-				<h3>Name</h3>
-			</div>
-			<div class="large-4 columns">
-				<h3>Band</h3>
-			</div>
-			<div class="large-2 columns">
-			</div>			
 		</div>
-			<?php foreach($shop_modifiers as $modifier): ?>
-			<div class="row table">
-				<div class="large-2 columns">
-					<p><?php echo $modifier['multiplier']; ?> x</p>
-				</div>
-				<div class="large-4 columns">
-					<p><?php echo $modifier['modifierName']; ?></p>
-				</div>
-				<div class="large-4 columns">
-					<p><?php echo $modifier['bandName']; ?></p>
-				</div>
-				<div class="large-2 columns">
-					<ul class="button-group even-2">
-						<li><?php echo anchor('/admin/shop/edit_modifier/'.$modifier['modifierID'], 'Edit', 'class="showform"'); ?></li>
-						<li><?php echo anchor('/admin/shop/delete_modifier/'.$modifier['modifierID'], 'Delete', 'onclick="return confirm(\'Are you sure you want to delete this?\');"'); ?></li>
-					</ul>
-				</div>
-			</div>
-			<?php endforeach; ?>
-
-		<?php else: ?>
-
-		<p>You have not yet set up any shipping modifiers yet.</p>
-
-		<?php endif; ?>
 	</div>
 </div>
