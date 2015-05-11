650b579be528b8da994ae6dacd90e6a47ea4175a
Jesse Orndorff <jesse@jesseorndorff.com>
2015-05-11 13:48:47 +0700

    fixing index.php system path

--- index.php
+++ index.php
@@ -98,7 +98,7 @@ switch (ENVIRONMENT)
  * Include the path if the folder is not in the same directory
  * as this file.
  */
-	$system_path = 'halogy';
+	$system_path = '/system';
 
 /*
  *---------------------------------------------------------------
@@ -113,7 +113,7 @@ switch (ENVIRONMENT)
  *
  * NO TRAILING SLASH!
  */
-	$application_folder = '/system';
+	$application_folder = 'halogy';
 
 /*
  *---------------------------------------------------------------
@@ -128,7 +128,7 @@ switch (ENVIRONMENT)
  *
  * NO TRAILING SLASH!
  */
-	$view_folder = '';
+	$view_folder = 'views';
 
 
 /*
